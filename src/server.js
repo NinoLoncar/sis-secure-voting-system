5503130c522fed4398e4d9f40161af86afab8b74
NinoLoncar <nino.loncar16@gmail.com>
2024-12-02 19:49:39 +0100

    Add serving of static resources

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
-express = require("express");
+const express = require("express");
+const path = require("path");
 
 const server = express();
 const port = 12000;
@@ -6,6 +7,17 @@ const port = 12000;
 startServer();
 
 function startServer() {
+  server.use("/css", express.static(path.join(__dirname, "../public/css")));
+  server.use("/js", express.static(path.join(__dirname, "../public/js")));
+  server.use(
+    "/images",
+    express.static(path.join(__dirname, "../public/images"))
+  );
+
+  server.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/html/index.html"));
+  });
+
   server.use((req, res) => {
     res.status(404);
     res.json({ message: "wrong url" });
