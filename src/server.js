0fca2df049f516bcdd3781ffa4c8a20d5fe2b5be
NinoLoncar <nino.loncar16@gmail.com>
2024-12-03 21:25:41 +0100

    Refactor server

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const authenticationService = require("./services/authenticationService.js");
 
 const server = express();
 const port = 12000;
@@ -7,16 +8,10 @@ const port = 12000;
 startServer();
 
 function startServer() {
-  server.use("/css", express.static(path.join(__dirname, "../public/css")));
-  server.use("/js", express.static(path.join(__dirname, "../public/js")));
-  server.use(
-    "/images",
-    express.static(path.join(__dirname, "../public/images"))
-  );
-
-  server.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/html/index.html"));
-  });
+  configureServer();
+  serveStaticFiles();
+  serveHtml();
+  serveServices();
 
   server.use((req, res) => {
     res.status(404);
@@ -25,5 +20,29 @@ function startServer() {
 
   server.listen(port, () => {
     console.log(`Server pokrenut na portu: ${port}`);
+  });
+}
+
+function configureServer() {
+  server.use(express.urlencoded({ extended: true }));
+  server.use(express.json());
+}
+
+function serveStaticFiles() {
+  server.use("/css", express.static(path.join(__dirname, "../public/css")));
+  server.use("/js", express.static(path.join(__dirname, "../public/js")));
+  server.use(
+    "/images",
+    express.static(path.join(__dirname, "../public/images"))
+  );
+}
+
+function serveServices() {
+  server.post("/login", authenticationService.login);
+}
+
+function serveHtml() {
+  server.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/html/index.html"));
   });
 }
