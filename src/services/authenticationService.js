acd4c1f2eb44e819c7a89aa536752aab9567cc59
NinoLoncar <nino.loncar16@gmail.com>
2024-12-03 21:24:19 +0100

    Create simple authentication service

---
+++ src/services/authenticationService.js
@@ -0,0 +1,44 @@
+const UserDAO = require("../db/daos/votersDao.js");
+const bycript = require("../utils/encryption/bcryptHasher.js");
+
+exports.login = async function (req, res) {
+  let userDao = new UserDAO();
+  let userData = req.body;
+  res.type("application/json");
+
+  if (!userData.username || !userData.password) {
+    return400(res, "Missing password or username");
+    return;
+  }
+
+  let existingUser = await userDao.getVoterByUsername(userData.username);
+
+  if (!existingUser) {
+    return401(res);
+    return;
+  }
+
+  let validPassword = bycript.verifyPassword(
+    userData.password,
+    existingUser.password
+  );
+
+  if (validPassword) {
+    //TODO
+    // Kreiranje sesije i JWT-a
+    res.status(200);
+    res.send(JSON.stringify({ message: "Successful login" }));
+  } else {
+    return401(res);
+  }
+};
+
+function return400(res, message) {
+  res.status(400);
+  res.send(JSON.stringify({ error: message }));
+}
+
+function return401(res) {
+  res.status(401);
+  res.send(JSON.stringify({ error: "Wrong credentials" }));
+}
