b57450a0c927f2b1df71177e141372662f21753e
kmilos-foi <kmilos21@foi.hr>
2024-12-04 20:29:18 +0100

    Add image_url to candidates

--- public/js/index.js
+++ public/js/index.js
@@ -13,40 +13,61 @@ function populateVotingOptions(candidates) {
     votingContainer.innerHTML = '';
 
     candidates.forEach(candidate => {
-        const card = document.createElement('div');
-        card.classList.add('card', 'card-width');
-
-        const imgContainer = document.createElement('div');
-        imgContainer.classList.add('img-container');
-        const img = document.createElement('img');
-        img.classList.add('card-img-top', 'image-style');
-        img.src = '../images/' + candidate.last_name + '.png';
-        img.alt = `Image of ${candidate.first_name}`;
-        imgContainer.appendChild(img);
-
-        const ul = document.createElement('ul');
-        ul.classList.add('list-group', 'list-group-flush');
-
-        const nameItem = document.createElement('li');
-        nameItem.classList.add('list-group-item');
-        nameItem.textContent = `Name: ${candidate.first_name} ${candidate.last_name}`;
-        ul.appendChild(nameItem);
-
-        const partyItem = document.createElement('li');
-        partyItem.classList.add('list-group-item');
-        partyItem.textContent = `Party: ${candidate.party}`;
-        ul.appendChild(partyItem);
-
-        const voteButton = document.createElement('button');
-        voteButton.type = 'submit';
-        voteButton.classList.add('btn', 'btn-primary', 'w-100');
-        voteButton.textContent = 'VOTE';
-        voteButton.addEventListener('click', () => handleVote(candidate.id));
-
-        card.appendChild(imgContainer);
-        card.appendChild(ul);
-        card.appendChild(voteButton);
+        const card = createCard(candidate);
         votingContainer.appendChild(card);
-
     });
+}
+
+function createCard(candidate) {
+    const card = document.createElement('div');
+    card.classList.add('card', 'card-width');
+
+    const imgContainer = createImageContainer(candidate);
+    const detailsList = createDetailsList(candidate);
+    const voteButton = createVoteButton(candidate);
+
+    card.appendChild(imgContainer);
+    card.appendChild(detailsList);
+    card.appendChild(voteButton);
+
+    return card;
+}
+
+function createImageContainer(candidate) {
+    const imgContainer = document.createElement('div');
+    imgContainer.classList.add('img-container');
+
+    const img = document.createElement('img');
+    img.src = candidate.image_url;
+    img.alt = 'candidate image';
+
+    imgContainer.appendChild(img);
+    return imgContainer;
+}
+
+function createDetailsList(candidate) {
+    const ul = document.createElement('ul');
+    ul.classList.add('list-group', 'list-group-flush');
+
+    const nameItem = document.createElement('li');
+    nameItem.classList.add('list-group-item');
+    nameItem.textContent = `Name: ${candidate.first_name} ${candidate.last_name}`;
+    ul.appendChild(nameItem);
+
+    const partyItem = document.createElement('li');
+    partyItem.classList.add('list-group-item');
+    partyItem.textContent = `Party: ${candidate.party}`;
+    ul.appendChild(partyItem);
+
+    return ul;
+}
+
+function createVoteButton(candidate) {
+    const voteButton = document.createElement('button');
+    voteButton.type = 'submit';
+    voteButton.classList.add('btn', 'btn-primary', 'w-100');
+    voteButton.textContent = 'VOTE';
+    voteButton.addEventListener('click', () => handleVote(candidate.id));
+
+    return voteButton;
 }
--- src/db/script.sql
+++ src/db/script.sql
@@ -10,8 +10,15 @@ password TEXT NOT NULL,
 voted BOOLEAN NOT NULL
 );
 
+ALTER TABLE candidates
+ADD COLUMN image_url TEXT DEFAULT NULL;
+
+
+
+DELETE FROM voters;
 INSERT INTO voters (username, email, password, voted) 
-VALUES ('admin','admin','admin', FALSE);
+VALUES ('admin','admin','$2a$12$w4MoQkvClE3iv6.oCp9hue74ZYSNqVkg8h8PEt7xLJ4EtyaYiw.aq', FALSE);
+
 
 CREATE TABLE votes (
 id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -26,12 +33,40 @@ last_name varchar(100) NOT NULL,
 party varchar(150) NOT NULL
 );
 
-INSERT INTO candidates (first_name, last_name, party) 
-VALUES ('Walter','White','The danger'), 
-('Gustavo','Fring','Los pollos hermanos'),
-('Hank','Schrader','DEA'),
-('Hector','Salamanca','Cartel');
+INSERT INTO candidates (username, email, password, voted) 
+VALUES ('admin','admin','admin', FALSE);
+
+
+
+
+INSERT INTO candidates (first_name, last_name, party, image_url) 
+VALUES ('Walter','White','The danger','https://i.pinimg.com/originals/b7/76/a7/b776a7801a57a22a00b8859f6e2c746c.jpg'),
+('Gustavo','Fring','Los pollos hermanos', 'https://i.namu.wiki/i/euH86A9h57zvltz6Vn9PaB-R1lXomWwf2DAMJdUXZvvAMbT9BN4qjXlOLSbXNdVVZwMSPkRLpMTiYekBTf7Uxg.webp'),
+('Hank','Schrader','DEA', 'https://images.genius.com/b0e98e4337eb9db45433b092a409b10b.1000x1000x1.png'),
+('Hector','Salamanca','Cartel', 'https://i.namu.wiki/i/h0yJ3FsmlGFPDUoTNbXGzFXsu6KmSiO_L95xqPDgQZKaEG60pvL79BmFfuAbajxYJKSdOjlXYWvbPzgEEr28aA.webp');
+
+DELETE FROM candidates;
+
+UPDATE candidates
+SET image_url = 'https://pbs.twimg.com/profile_images/378800000297781379/6395cb24dc09c37c938ffc541d0290d4_400x400.jpeg'
+WHERE first_name = 'Hector';
 
 
 SELECT * FROM voters;
 SELECT * FROM candidates;
+
+
+
+-- 1. Kreiranje nove tabele bez kolone `image_url`
+CREATE TABLE voters_new AS
+SELECT id, username, email, password, voted
+FROM voters;
+
+-- 2. Preimenovanje stare tabele za sigurnost (opciono)
+ALTER TABLE voters RENAME TO voters_old;
+
+-- 3. Preimenovanje nove tabele na ime originalne
+ALTER TABLE voters_new RENAME TO voters;
+
+-- 4. Opcionalno: Brisanje stare tabele ako je sve ispravno prebačeno
+DROP TABLE voters_old;
