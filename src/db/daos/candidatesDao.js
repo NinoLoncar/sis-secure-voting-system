871eaeabf9685f0274c2cd410f5f501cc271c326
NinoLoncar <nino.loncar16@gmail.com>
2024-12-06 14:07:32 +0100

    Update candidatesDao.js

--- src/db/daos/candidatesDao.js
+++ src/db/daos/candidatesDao.js
@@ -20,6 +20,20 @@ class CandidatesDao {
     if (data.length == 1) return data[0];
     else return null;
   };
+
+  getCandidatesCount = async function () {
+    this.db.connect();
+    let sql = "SELECT COUNT(*) AS candidateCount FROM candidates";
+    var result = await this.db.executeQuery(sql, []);
+    this.db.disconnect();
+    return result[0].candidateCount;
+  };
+  saveVoteCount = async function (id, count) {
+    this.db.connect();
+    let sql = "UPDATE candidates SET vote_count = ? WHERE id = ?;";
+    var result = await this.db.executeQuery(sql, [id, count]);
+    this.db.disconnect();
+  };
 }
 
 module.exports = CandidatesDao;
