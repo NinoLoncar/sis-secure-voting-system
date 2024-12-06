bb3029f7c6b5ce04ed566f96b7d14c74990047de
NinoLoncar <nino.loncar16@gmail.com>
2024-12-06 14:07:06 +0100

    Update script.sql

--- src/db/script.sql
+++ src/db/script.sql
@@ -10,33 +10,24 @@ password TEXT NOT NULL,
 voted BOOLEAN NOT NULL
 );
 
-ALTER TABLE candidates
-ADD COLUMN image_url TEXT DEFAULT NULL;
-
-
-
 DELETE FROM voters;
 INSERT INTO voters (username, email, password, voted) 
-VALUES ('admin','karlomilos7@gmail.com','$2a$12$w4MoQkvClE3iv6.oCp9hue74ZYSNqVkg8h8PEt7xLJ4EtyaYiw.aq', FALSE);
-
+VALUES ('admin','fusion.project.management.app@gmail.com','$2a$12$w4MoQkvClE3iv6.oCp9hue74ZYSNqVkg8h8PEt7xLJ4EtyaYiw.aq', FALSE);
 
 CREATE TABLE votes (
 id INTEGER PRIMARY KEY AUTOINCREMENT,
 signature TEXT NOT NULL,
-vote NOT NULL
+encrypted_vote NOT NULL
 );
 
 CREATE TABLE candidates (
 id INTEGER PRIMARY KEY AUTOINCREMENT,
 first_name varchar(100) NOT NULL,
 last_name varchar(100) NOT NULL,
-party varchar(150) NOT NULL
+party varchar(150) NOT NULL,
+image_url TEXT DEFAULT NULL,
+vote_count INTEGER DEFAULT 0
 );
-
-INSERT INTO voters (username, email, password, voted) 
-VALUES ('user','karlomilos7@gmail.com','admin', FALSE);
-
-
 
 
 INSERT INTO candidates (first_name, last_name, party, image_url) 
@@ -45,28 +36,7 @@ VALUES ('Walter','White','The danger','https://i.pinimg.com/originals/b7/76/a7/b
 ('Hank','Schrader','DEA', 'https://images.genius.com/b0e98e4337eb9db45433b092a409b10b.1000x1000x1.png'),
 ('Hector','Salamanca','Cartel', 'https://i.namu.wiki/i/h0yJ3FsmlGFPDUoTNbXGzFXsu6KmSiO_L95xqPDgQZKaEG60pvL79BmFfuAbajxYJKSdOjlXYWvbPzgEEr28aA.webp');
 
-DELETE FROM candidates;
-
-UPDATE candidates
-SET image_url = 'https://wallpaperaccess.com/full/9176760.jpg'
-WHERE first_name = 'Hector';
-
 
 SELECT * FROM voters;
 SELECT * FROM candidates;
-
-
-
--- 1. Kreiranje nove tabele bez kolone `image_url`
-CREATE TABLE voters_new AS
-SELECT id, username, email, password, voted
-FROM voters;
-
--- 2. Preimenovanje stare tabele za sigurnost (opciono)
-ALTER TABLE voters RENAME TO voters_old;
-
--- 3. Preimenovanje nove tabele na ime originalne
-ALTER TABLE voters_new RENAME TO voters;
-
--- 4. Opcionalno: Brisanje stare tabele ako je sve ispravno prebačeno
-DROP TABLE voters_old;
+SELECT * FROM votes;
